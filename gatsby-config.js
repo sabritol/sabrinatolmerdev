34d50b24e35f39de37741fd2a8515813c4266320
Sabritol <sabrinatolmer@gmail.com>
2022-01-03 19:04:24 +0100

    fix: hygiene

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,18 +21,6 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "GitHub",
-        fieldName: "github",
-        url: "https://api.github.com/graphql",
-        headers: {
-          Authorization: `bearer ${process.env.PORTFOLIO_GITHUB_TOKEN}`,
-        },
-        fetchOptions: {},
-      },
-    },
-    {
       resolve: "gatsby-plugin-nprogress",
       options: {
         color: config.themeColor,
