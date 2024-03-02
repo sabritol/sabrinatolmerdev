51d7c95ca1752c12162f946396f971c2863c2656
sabritol <sabrinatolmer@gmail.com>
2024-03-02 13:15:41 -0300

    fixes

--- src/components/landing/Contact/ContactForm/styles.js
+++ src/components/landing/Contact/ContactForm/styles.js
@@ -13,6 +13,23 @@ export const Center = styled.div`
 `;
 
 export const InputField = styled.div`
-  position: relative;
+  /* position: relative; */
   margin-bottom: 1rem;
+
+  box-shadow:
+    4px 4px 0 #ba5b45,
+    4.8px 4.8px 0 #000,
+    3px 3px 0 #000,
+    4.8px 3px 0 #000,
+    3px 4.8px 0 #000,
+    8px 8px 0 #dbbb83,
+    9px 9px #000,
+    7px 7px #000,
+    7px 9px #000,
+    9px 7px #000,
+    12px 12px #ece99c,
+    13px 13px #000,
+    11px 11px #000,
+    13px 11px #000,
+    11px 13px #000;
 `;
